13c5f00dd5b081aecb7fab30a001a2c45b7786f4
Kusal <debnathk1997@gmail.com>
2023-09-03 19:58:47 -0400

    Another commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,48 @@
+import java.awt.*;
+import java.util.Date;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+//        # Firs java code
+    //    System.out.println("Hello and welcome!");
+
+//        # Variables
+    //    int age = 30;
+//        age = 25;
+//        int newAge = age;
+    //    System.out.println(newAge);
+
+//        # Types
+//        ## Primitive Types - Doesn't need memory allocation
+    //    byte age = 30;
+    //    long viewsCount = 3_123_456_789L;
+    //    float price = 10.99F;
+    //    char letter = 'A';
+    //    boolean isEligible = false;
+
+//        ## Reference Types - Does need memory allocation
+//        Date now = new Date();
+//        System.out.println(now);
+
+//        ## Reference Types vs Primitive Types
+//        ### Primitive types
+//        byte x = 1;
+//        byte y = 2;
+//        y = 1;
+//        System.out.println(y); // As primitive types, change of value for y doesn't affect the value of x
+
+//        ### Reference Types
+//        Point point1 = new Point(1, 1);
+//        Point point2 = point1;
+//        point1.x = 2;
+//        System.out.println(point2); // As reference types, point1 and point2 are referring to the same memory address,
+//        // Thus, changing the values stored in the point1 is equal to changing the values stored in the point2
+
+
+
         }
 }
