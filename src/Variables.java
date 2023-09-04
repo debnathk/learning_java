0eeb7f3cd0608d879e8b4de3114066ebe58f03c8
Kusal <debnathk1997@gmail.com>
2023-09-03 20:15:12 -0400

    Another commit

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 30;
+        age = 25;
+        int newAge = age;
+        System.out.println(newAge);
+    }
+}
