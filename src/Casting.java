eef647a520bd8aeff4cda391fb26cd511445a871
Kusal <debnathk1997@gmail.com>
2023-09-04 10:02:10 -0400

    Another commit

--- src/Casting.java
+++ src/Casting.java
@@ -1,2 +1,19 @@
 public class Casting {
+    public static void main (String[] args) {
+//        Implicit Casting (automatic)
+//        byte > short > int > long
+        short x = 1;
+        int y = x + 2; // casted short to int
+        System.out.println(y);
+
+//        Explicit casting (manual)
+        double a = 1.1;
+        int b = (int)a + 2;
+        System.out.println(b);
+
+//        Parsing
+        String s = "1";
+        int t = Integer.parseInt(s) + 2;
+        System.out.println(t);
+    }
 }
