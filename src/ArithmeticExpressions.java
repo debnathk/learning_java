d4f9e64e262b36848968f24f2c4e4f4899fc5243
Kusal <debnathk1997@gmail.com>
2023-09-03 22:02:53 -0400

    Another commit

---
+++ src/ArithmeticExpressions.java
@@ -0,0 +1,12 @@
+public class ArithmeticExpressions {
+    public static void main (String[] args) {
+        double div = (double)10 / (double)3;
+        System.out.println(div);
+
+        byte x = 1;
+        System.out.println(x);
+        x = ++x;
+        System.out.println(x);
+
+    }
+}
