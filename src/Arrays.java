4fea7f3822e6b131cf58eed497d4d0beb9fb75c9
Kusal <debnathk1997@gmail.com>
2023-09-03 21:25:42 -0400

    Another commit

--- src/Arrays.java
+++ src/Arrays.java
@@ -4,5 +4,9 @@ public class Arrays {
         System.out.println(java.util.Arrays.toString(numbers));
         java.util.Arrays.sort(numbers);
         System.out.println(java.util.Arrays.toString(numbers));
+
+//        Multi-dimensional arrays
+        int[][] numbers2D = {{1,0,2}, {3,0,4}};
+        System.out.println(java.util.Arrays.deepToString(numbers2D));
     }
 }
