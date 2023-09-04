a5b618441985cde02fbcdc3e2e92746f5e3a60c7
Kusal <debnathk1997@gmail.com>
2023-09-03 21:17:07 -0400

    Another commit

---
+++ src/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main (String[] args) {
+        int[] numbers = {2, 1, 3, 5, 4};
+        System.out.println(java.util.Arrays.toString(numbers));
+        java.util.Arrays.sort(numbers);
+        System.out.println(java.util.Arrays.toString(numbers));
+    }
+}
