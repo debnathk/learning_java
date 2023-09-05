e262637c4e6f5c1568295402fb74074370841383
debnathk <debnathk1997@gmail.com>
2023-09-05 16:19:32 -0400

    Another commit

--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class WhileLoops {
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
 
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
     }
 }
