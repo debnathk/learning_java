46254446484d7cb01fac2b01f65047b209c73a08
debnathk <debnathk1997@gmail.com>
2023-09-06 11:17:12 -0400

    Another commit

---
+++ src/BeakAndContinue.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BeakAndContinue {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            System.out.print("Input: ");
+            input = scanner.next();
+            if (input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
+            System.out.println(input);
+        }
+    }
+}
