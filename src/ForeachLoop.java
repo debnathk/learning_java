1f4d639da927c6a4e0f32d5a4d00ed6b83027e33
debnathk <debnathk1997@gmail.com>
2023-09-06 11:30:03 -0400

    Another commit

---
+++ src/ForeachLoop.java
@@ -0,0 +1,8 @@
+public class ForeachLoop {
+    public static void main (String[] args) {
+        String[] letters = {"A", "B", "C"};
+        for (String letter : letters) {
+            System.out.println(letter);
+        }
+    }
+}
